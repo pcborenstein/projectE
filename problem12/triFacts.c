adedc4996f4421e2607306d2c1fb8d54350c55f3
Peter <pcborenstein@gmail.com>
2018-05-23 23:38:44 -0400

    optimized problem 12

--- problem12/triFacts.c
+++ problem12/triFacts.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <time.h>
+#include <math.h>
 
 int main(void){
 
@@ -19,30 +20,21 @@ int main(void){
     do{
         i++;
         triNum += i;
-        numFactors = 0;
 
         /*
         Every factor has a pair; 1 and the number itself,
         possibly 2 and half the number, possibly 3 and a third of the number (triNum).
         By identifying the maximum possible factor, I can reduce runtime.
-        This optimization will prevent checking all numbers above triNum divided
-        by the first found factor.
+        The first of all these pairs is at least less than the square root of the number.
         */
-        uint32_t maxPossibleFactor = triNum;
-        bool firstFactorFound = false;
+        //Start at 2 because we count a number divided by itself and 1  in this problem
+        numFactors = 1;
+        uint32_t maxPossibleFactor = (uint32_t)sqrt(triNum);
         for(j = 2; j<= maxPossibleFactor; j++){
-            if((triNum % j) == 0){
+            if((triNum % j) == 0)
                 numFactors++;
-                if(firstFactorFound == false){
-                    firstFactorFound = true;
-                    maxPossibleFactor = triNum/j;
-                }
-            }else if(firstFactorFound == false){
-                maxPossibleFactor = triNum / (j + 1);
-            }
         }
-        //Add two more because we count a number divided by itself and 1  in this problem
-        numFactors += 2;
+        numFactors *= 2;
         if(numFactors > maxNumFactors){
             maxNumFactors = numFactors;
         }
